b983e3f275c087fa899a2fa0ec127fcdcfeac003
surfd4wg <63609504+surfd4wg@users.noreply.github.com>
2022-05-31 17:43:51 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,9 +10,12 @@ terraform {
   #     read the README.md first
   backend "s3" {
     # Make sure the exact bucket name exists before running this
-    bucket         = "cequence-tf-state"
+    # You would have to create the bucket beforehand, where "<your bucket name>=mybucket-tf-state" for example
+    bucket         = "<your bucket name>-tf-state"
     key            = "tf-cequence-backend/terraform.tfstate"
-    dynamodb_table = "cequence-tf-state"
+    # Running this the first time, it will create the DynamoDB table through the terraform run
+    # Make sure the DynamoDB table name is unique, where "<your table name>=mytable.tf-state" for example
+    dynamodb_table = "<your table name>-tf-state"
     profile        = "default"
     region         = "us-east-1"
   }
